775a8af8c3036ca2bbf2369961e3425c62b41a02
sSs_Kaneki_ken <ssskakashi2@gmail.com>
2024-11-09 19:33:47 +0500

    Update index.blade.php

--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -2,45 +2,49 @@
 
 @section('content')
     <section class="content">
-      <div class="container-fluid">
-        <!-- Info boxes -->
-      <div class="container mt-5">
+        <div class="container-fluid">
+            <!-- Info boxes -->
+            <div class="container mt-5">
 
-        <h1 class="mb-3">Students</h1>
+                <h1 class="mb-3">Students</h1>
 
-        <a href="#" class="btn btn-primary mb-4">Create</a>
-        <table class="table">
-            <thead>
-              <tr>
-                <th scope="col">Id</th>
-                <th scope="col">Name</th>
-                <th scope="col">Age</th>
-                <th scope="col">Phone</th>
-                <th scope="col">Delete</th>
-                <th scope="col">Update</th>
-              </tr>
-            </thead>
-            <tbody>
-                @foreach ($students as $student)
-                    <tr>
-                        <th>{{$student->id}}</th>
-                        <th>{{$student->name}}</th>
-                        <th>{{$student->age}}</th>
-                        <th>{{$student->phone}}</th>
-                        <th>
-                            <form action="{{ route('student.destroy', $student->id) }}" method="POST">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="btn btn-danger">Delete</button>
-                            </form>
-                             <th><a href="#" class="btn btn-warning">Update</a>
-                            </th>
+                <a href="{{ route('student.create') }}" class="btn btn-primary mb-4">Create</a>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Id</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Age</th>
+                            <th scope="col">Phone</th>
+                            <th scope="col">Delete</th>
+                            <th scope="col">Update</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($students as $student)
+                            <tr>
+                                <td>{{ $student->id }}</td>
+                                <td>{{ $student->name }}</td>
+                                <td>{{ $student->age }}</td>
+                                <td>{{ $student->phone }}</td>
+                                <td>
+                                    <form action="{{ route('student.destroy', $student->id) }}" method="POST">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-danger">Delete</button>
+                                    </form>
+                                </td>
+                                <td>
+                                    <a href="{{ route('student.edit', $student->id) }}" class="btn btn-warning">Update</a>
+                                </td>
                             </tr>
-                @endforeach
-            </tbody>
-          </table>
-          {{ $students->links('pagination::bootstrap-4') }}
-    </div>
-    </section>
+                        @endforeach
+                    </tbody>
+                </table>
 
+                <!-- Pagination links -->
+                {{ $students->links('pagination::bootstrap-4') }}
+            </div>
+        </div>
+    </section>
 @endsection
